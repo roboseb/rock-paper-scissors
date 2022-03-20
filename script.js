805d242809eff4b87835adc7a64cf147544f5265
roboseb <sebastienapril@rocketmail.com>
2022-03-20 13:47:00 -0600

    index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    let comChoices = ["rock","paper","scissors"];
+    let comPick = comChoices[Math.floor(Math.random()*3)];
+    return comPick;
+}
+
+function playRound() {
+    
+}
